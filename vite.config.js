5641fb2f1a60c9ae431e4fa6f5a5e73bcb140311
Mohit <mohittopiya2564@gmail.com>
2026-04-20 21:16:49 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react({
+      // This ensures dev helpers like $RefreshSig$ are never injected in production
+      fastRefresh: false 
+    })
   ],
   server: {
     host: '0.0.0.0',
